3ad64ab0e235223d802b79f31ec736066acbc5b9
rami-platero <ramiplatero11@gmail.com>
2026-09-24 15:00:29 -0300

    feat(portfolio): render portfolio page in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer/Footer";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,17 @@
+import Contact from "./Sections/Contact";
+import Hero from "./Sections/Hero";
+import Projects from "./Sections/Projects";
+import Skills from "./Sections/Skills";
+
+const Portfolio = () => {
+  return (
+    <main className="flex w-full max-w-[1400px] flex-col gap-[150px] p-4">
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
+};
+
+export default Portfolio;
